a28c93bd4716f93a5545f8fb74032c7f5bbeb56b
RealThomasMiles <Dmilya2000@gmail.com>
2021-04-12 21:42:13 +0300

    Implemented the "get featured playlists" request

--- src/advisor/strategy/StrategyGetFeatured.java
+++ src/advisor/strategy/StrategyGetFeatured.java
@@ -1,17 +1,59 @@
 package advisor.strategy;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StrategyGetFeatured implements Strategy {
-    private final List<String> featuredPlaylists = List.of("Mellow Morning",
-            "Wake Up and Smell the Coffee",
-            "Monday Motivation",
-            "Songs to Sing in the Shower"
-    );
-
     @Override
     public List<String> execute(HttpClient httpClient, String resourceAPI, String accessToken) {
+        List<String> featuredPlaylists = new ArrayList<>();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .header("Authorization", "Bearer " + accessToken)
+                .uri(URI.create(resourceAPI + "/v1/browse/featured-playlists"))
+                .GET()
+                .build();
+
+        HttpResponse<String> httpResponse;
+
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        JsonObject responseObject = JsonParser.parseString(httpResponse.body()).getAsJsonObject();
+
+        JsonArray playlists;
+
+        if (responseObject.has("playlists")) {
+            playlists = responseObject.get("playlists").getAsJsonObject().get("items").getAsJsonArray();
+        } else {
+            System.out.println(responseObject.get("error").getAsJsonObject().get("message"));
+            return null;
+        }
+
+        for (JsonElement playlist : playlists) {
+            JsonObject playlistObject = playlist.getAsJsonObject();
+
+            String name = playlistObject.get("name").getAsString();
+
+            String url = playlistObject.get("external_urls").getAsJsonObject().get("spotify").getAsString();
+
+            featuredPlaylists.add(name + "\n" + url + "\n");
+        }
+
         return featuredPlaylists;
     }
 }
